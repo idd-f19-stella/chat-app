c5ff50bbba1acb0383ac82c71a85cd1920f5af50
graphic <graphic@wfd306-28m.gbc.local>
2019-12-11 14:52:46 -0500

    sec

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const user = {
+    id: 456,
+    name: 'Charles Babbage',
+    img: '/img/girl2.jpg'
+ }
+ 
+ const messages = [
+    {
+       id: 1,
+       from: {
+          id: 123,
+          name: 'Ada Lovelace',
+          img: '/img/girl1.jpg'
+       },
+       time: {
+          date: 1,
+          month: 8,
+          year: 1843,
+          hour: 14,
+          minute: 59
+       },
+       text: `You should check out this little script I just wrote. 😂 lol`
+    }
+ ]
